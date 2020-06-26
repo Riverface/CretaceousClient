0dcf6052fe8158c1f5302dc2e485613646628c3f
Riverface <sonoskeakigawa@gmail.com>
2020-06-26 11:19:34 -0700

    Reviewed and implemented proper CRUD

--- CretaceousClient/Controllers/AnimalsController.cs
+++ CretaceousClient/Controllers/AnimalsController.cs
@@ -6,15 +6,56 @@ namespace CretaceousClient.Controllers
 {
     public class AnimalsController : Controller
     {
+        // /animals
         public IActionResult Index()
         {
             List<Animal> allAnimals = Animal.GetAnimals();
             return View(allAnimals);
         }
+
+        // /animals/details/id
         public IActionResult Details(int id)
         {
-        Animal animal = Animal.GetDetails(id);
-        return View(animal);
+            Animal animal = Animal.GetDetails(id);
+            return View(animal);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Animal animal = Animal.GetDetails(id);
+            return View(animal);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Animal animal)
+        {
+            Animal.Put(animal);
+            return RedirectToAction("Details", "Animals", new { id = animal.AnimalId });
+        }
+
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult New(Animal animal)
+        {
+            Animal.Post(animal);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Animal animal = Animal.GetDetails(id);
+            return View(animal);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            Animal.Delete(id);
+            return RedirectToAction("Index", "Animals");
         }
     }
 }
--- CretaceousClient/Models/Animal.cs
+++ CretaceousClient/Models/Animal.cs
@@ -23,6 +23,7 @@ namespace CretaceousClient.Models
 
             return animalList;
         }
+
         public static Animal GetDetails(int id)
         {
             var apiCallTask = ApiHelper.Get(id);
@@ -32,16 +33,19 @@ namespace CretaceousClient.Models
             Animal animal = JsonConvert.DeserializeObject<Animal>(jsonResponse.ToString());
             return animal;
         }
+
         public static void Post(Animal animal)
         {
             string jsonAnimal = JsonConvert.SerializeObject(animal);
             var apiCallTask = ApiHelper.Post(jsonAnimal);
         }
+
         public static void Put(Animal animal)
         {
             string jsonAnimal = JsonConvert.SerializeObject(animal);
             var apiCallTask = ApiHelper.Put(animal.AnimalId, jsonAnimal);
         }
+
         public static void Delete(int id)
         {
             var apiCallTask = ApiHelper.Delete(id);
